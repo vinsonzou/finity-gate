1d54cc58cd008870d626deb62fa20545c67ead28
walker <18622283215@163.com>
2016-01-21 16:36:07 +0800

    add server list

--- sql/player.sql
+++ sql/player.sql
@@ -1,10 +1,9 @@
 CREATE TABLE player
 (
   userid INT NOT NULL,
-  gameid INT NOT NULL,
   serverid INT NOT NULL,
   playerid INT NOT NULL,
   /*TODO player payloads*/
-  PRIMARY KEY(userid, gameid, serverid)
+  PRIMARY KEY(userid, serverid)
 )
 ENGINE = INNODB DEFAULT CHARACTER SET UTF8;
